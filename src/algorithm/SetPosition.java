4811c8aa5756a4823e813cd58f8cfe936aa5d344
moontaijin <moontaijin@naver.com>
2018-06-02 15:48:42 +0900

    Update SetPosition.java
    Add angle algorithm

--- src/algorithm/SetPosition.java
+++ src/algorithm/SetPosition.java
@@ -1,6 +1,7 @@
 package algorithm;
 import IO.treeIO;
 import layout.MainLayout;
+import sun.nio.cs.ext.MacThai;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
@@ -34,17 +35,42 @@ public class SetPosition {
 
         q.offer(now_node);
 
+        now_node.setX(1280/2);
+        now_node.setY(640/2);
+
         while (true){
             now_node=(IO.treeIO)q.peek();
             int sibling_cnt=now_node.getChildCount();
 
-            now_node.setX(1);
-            now_node.setY(1);
-            now_node.setW(1);
-            now_node.setH(1);//여기 있는 것들은 차후 width,height값에 따라 변경
+            double X,Y;
+            int length=10;//여기 뭐 들어가야할지 그려보면서 실험
+            int cnt=0;
+            X=now_node.getX();
+            Y=now_node.getY();
+
+            int sibling_num=number_of_sibiling(now_node);
+            int num=now_node.get_child_number_in_parent();
+            double lim_angle=(num/sibling_num)*Math.PI;
 
             for (int i=0;i<sibling_cnt;i++){
+
+                if (now_node.getChildAt(i)==null)
+                    continue;
+                else
+                    cnt++;
+
+                double angle;
+
+                if (sibling_cnt==1)
+                    angle=lim_angle;
+                else
+                    angle=(lim_angle/2)+(lim_angle/(sibling_cnt-1))*i;
+
                 q.offer(now_node.getChildAt(i));
+                now_node.getChildAt(i).setX(X+Math.cos(angle)*length);
+                now_node.getChildAt(i).setY(Y+Math.sin(angle)*length);
+                now_node.getChildAt(i).setW(1);
+                now_node.getChildAt(i).setH(1);//여기 있는 것들은 차후 width,height값에 따라 변경
             }
 
             q.poll();
@@ -71,5 +97,12 @@ public class SetPosition {
         }
 
         return MAX;
+    }
+
+    public int number_of_sibiling(treeIO node){
+        if (node.get_child_number_in_parent()==-1)
+            return -1;
+        else
+            return node.getParent().getChildCount();
     }
 }
