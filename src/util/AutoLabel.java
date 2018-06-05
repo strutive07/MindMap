a25c4c034b93a2f5da8d27d4441383bedd228cee
strutive07 <strutive07@gmail.com>
2018-06-05 20:11:22 +0900

    오타 ㅈㅅ

--- src/util/AutoLabel.java
+++ src/util/AutoLabel.java
@@ -33,7 +33,7 @@ public class AutoLabel {
         while(true){
             if(label.getPreferredSize().getWidth() > size){
                 Font font = new Font(label.getFont().getName(), label.getFont().getStyle(), font_size--);
-                label.setFont(font);A
+                label.setFont(font);
             }else{
                 break;
             }
