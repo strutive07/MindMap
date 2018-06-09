f59c2d2238dc9c04403c0b68fa17216cfe123be3
moontaijin <moontaijin@naver.com>
2018-06-09 23:41:56 +0900

    SetPostion+getTreeHeight

--- src/algorithm/SetPosition.java
+++ src/algorithm/SetPosition.java
@@ -19,113 +19,100 @@ public class SetPosition {
     }
 
     public void start_SetPosition() {
-        if (root_list.size() == 1)
-            make_grahp();
-        else if (root_list.size()>=2)
-            make_tree();
+        make_grahp();
     }
-
-    public void make_tree(){
-        double limit_x=0;
-
-        for (int t=0;t<root_list.size();t++){
-            int tree_height=getTreeHeight(root_list.get(t));
-            int max_x=0;
-
-
-        }
-    }
-
 
     public void make_grahp(){
-        int child_cnt;
-        int tree_height=getTreeHeight(root_list.get(0));
-        Queue q=new LinkedList();
-        treeIO now_node=root_list.get(0);
 
-        q.offer(now_node);
+        for (int t=0;t<root_list.size();t++) {
+            int child_cnt;
+            int tree_height = getTreeHeight(root_list.get(t));
+            Queue q = new LinkedList();
+            treeIO now_node = root_list.get(t);
 
-        now_node.setX(panelWeight/2);
-        now_node.setY(panelHeight/2);
-        now_node.setW(50);
-        now_node.setH(31);
-        now_node.set_angle(0);
-        double Adj_X=0,Adj_Y=0;
+            q.offer(now_node);
 
-        while (!q.isEmpty()){
-            now_node=(IO.treeIO)q.peek();
-            int sibling_cnt=now_node.getChildCount();
+            now_node.setX(panelWeight / 2);
+            now_node.setY(panelHeight / 2);
+            now_node.setW(50);
+            now_node.setH(31);
+            now_node.set_angle(0);
+            double Adj_X = 0, Adj_Y = 0;
 
-            double X,Y;
-            int cnt=0;
-            X=now_node.getX();
-            Y=now_node.getY();
+            while (!q.isEmpty()) {
+                now_node = (IO.treeIO) q.peek();
+                int sibling_cnt = now_node.getChildCount();
 
-            int sibling_num=number_of_sibiling(now_node);
-            int num=now_node.get_child_number_in_parent();
-            double lim_angle=((double)1/(double)sibling_num+0.2)*Math.PI*2;
-            if (lim_angle>Math.PI*2.0/3.6)
-                lim_angle=Math.PI*2.0/3.6;
+                double X, Y;
+                int cnt = 0;
+                X = now_node.getX();
+                Y = now_node.getY();
 
-            int length=100+(int)(sibling_cnt*25/lim_angle);
+                int sibling_num = number_of_sibiling(now_node);
+                int num = now_node.get_child_number_in_parent();
+                double lim_angle = ((double) 1 / (double) sibling_num + 0.2) * Math.PI * 2;
+                if (lim_angle > Math.PI * 2.0 / 3.6)
+                    lim_angle = Math.PI * 2.0 / 3.6;
 
-            for (int i=0;i<sibling_cnt;i++){
-                double angle;
-                if (now_node.get_child_number_in_parent()==-1)
-                    angle=((double)i/(double)sibling_cnt)*Math.PI*2;
-                else {
-                    if (sibling_cnt == 1) {
-                        angle = now_node.get_angle();
-                    }
+                int length = 100 + (int) (sibling_cnt * 25 / lim_angle);
+
+                for (int i = 0; i < sibling_cnt; i++) {
+                    double angle;
+                    if (now_node.get_child_number_in_parent() == -1)
+                        angle = ((double) i / (double) sibling_cnt) * Math.PI * 2;
                     else {
-                        angle = ( now_node.get_angle()-(lim_angle/2.0))+ (lim_angle / (double)(sibling_cnt - 1)) * i;
+                        if (sibling_cnt == 1) {
+                            angle = now_node.get_angle();
+                        } else {
+                            angle = (now_node.get_angle() - (lim_angle / 2.0)) + (lim_angle / (double) (sibling_cnt - 1)) * i;
+                        }
                     }
+
+                    now_node.getChildAt(i).set_angle(angle);
+                    q.offer(now_node.getChildAt(i));
+                    now_node.getChildAt(i).setX(X + Math.cos(angle) * length);
+                    now_node.getChildAt(i).setY(Y + Math.sin(angle) * length);
+                    now_node.getChildAt(i).setW(50);
+                    now_node.getChildAt(i).setH(31);
+
+                    if (X + Math.cos(angle) * length < -Adj_X)
+                        Adj_X = -(X + Math.cos(angle) * length);
+
+                    if (Y + Math.sin(angle) * length < -Adj_Y)
+                        Adj_Y = -(Y + Math.sin(angle) * length);
+
                 }
 
-                now_node.getChildAt(i).set_angle(angle);
-                q.offer(now_node.getChildAt(i));
-                now_node.getChildAt(i).setX(X+Math.cos(angle)*length);
-                now_node.getChildAt(i).setY(Y+Math.sin(angle)*length);
-                now_node.getChildAt(i).setW(50);
-                now_node.getChildAt(i).setH(31);
-
-                if (X+Math.cos(angle)*length<-Adj_X)
-                    Adj_X=-(X+Math.cos(angle)*length);
-
-                if (Y+Math.sin(angle)*length<-Adj_Y)
-                    Adj_Y=-(Y+Math.sin(angle)*length);
-
+                q.poll();
             }
 
-            q.poll();
-        }
+            now_node = root_list.get(0);
+            q.offer(now_node);
 
-        now_node=root_list.get(0);
-        q.offer(now_node);
+            while (!q.isEmpty()) {
+                now_node = (treeIO) q.peek();
+                int num = now_node.getChildCount();
+                double X, Y;
+                X = now_node.getX();
+                Y = now_node.getY();
+                now_node.setX(X + Adj_X);
+                now_node.setY(Y + Adj_Y);
 
-        while(!q.isEmpty()){
-            now_node=(treeIO)q.peek();
-            int num=now_node.getChildCount();
-            double X,Y;
-            X=now_node.getX();
-            Y=now_node.getY();
-            now_node.setX(X+Adj_X);
-            now_node.setY(Y+Adj_Y);
+                for (int i = 0; i < num; i++) {
+                    q.offer(now_node.getChildAt(i));
+                }
 
-            for (int i=0;i<num;i++){
-                q.offer(now_node.getChildAt(i));
+                CenterPanel centerPanel = layout.MainLayout.getCenterPanel();
+                if (centerPanel.getPreferredSize().getHeight() < now_node.getY()) {
+                    centerPanel.setPreferredSize(new Dimension((int) centerPanel.getPreferredSize().getWidth(), (int) centerPanel.getPreferredSize().getHeight() * 2));
+                }
+
+                if (centerPanel.getPreferredSize().getWidth() < now_node.getX()) {
+                    centerPanel.setPreferredSize(new Dimension((int) centerPanel.getPreferredSize().getWidth() * 2, (int) centerPanel.getPreferredSize().getHeight()));
+                }
+
+                q.poll();
             }
-
-            CenterPanel centerPanel = layout.MainLayout.getCenterPanel();
-            if(centerPanel.getPreferredSize().getHeight() < now_node.getY()){
-                centerPanel.setPreferredSize(new Dimension((int)centerPanel.getPreferredSize().getWidth(), (int)centerPanel.getPreferredSize().getHeight() * 2 ));
-            }
-
-            if(centerPanel.getPreferredSize().getWidth() < now_node.getX()){
-                centerPanel.setPreferredSize(new Dimension((int)centerPanel.getPreferredSize().getWidth() * 2, (int)centerPanel.getPreferredSize().getHeight()));
-            }
-
-            q.poll();
         }
     }
 
